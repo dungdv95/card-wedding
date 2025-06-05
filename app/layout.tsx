570e91017666025bf35e84deac9cc5c725bdcd98
dungdv95 <dungdv95.bg@gmail.com>
2025-06-05 23:25:19 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
     description: "Wedding Dũng & Nhàn",
     images: [
       {
-        url: `https://card-wedding-dun.vercel.app/images/thumb1.jpg?v=${Date.now()}`,
+        url: `https://card-wedding-dun.vercel.app/images/thumb1.png`,
         alt: "Hình ảnh",
       },
     ],
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
     description: "Wedding Dũng & Nhàn",
     images: [
       {
-        url: `https://card-wedding-dun.vercel.app/images/thumb1.jpg?v=${Date.now()}`,
+        url: `https://card-wedding-dun.vercel.app/images/thumb1.png`,
         alt: "Hình ảnh",
       },
     ],
