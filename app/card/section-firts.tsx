6052609bf9a59d9319c52e3d1fdb33febdd4a4b2
dungdv95 <dungdv95.bg@gmail.com>
2025-06-06 15:51:38 +0700

    update

--- app/card/section-firts.tsx
+++ app/card/section-firts.tsx
@@ -5,8 +5,17 @@ export default function SectionFirts() {
   return (
     <section className="container mx-auto px-20 max-md:px-2 py-10">
       <div className="flex flex-col items-center">
-        <div className="max-sm:px-12">
-          <Image alt="wed" src="/images/wedd.png" width={400} height={200} />
+        <div className="relative">
+          <Image
+            alt="wed"
+            src="/images/ring.png"
+            width={60}
+            height={60}
+            className="absolute top-[-16px] left-[-23px] max-[38rem]:w-[40px] max-[38rem]:h-[40px] max-[38rem]:top-[-10px]"
+          />
+          <span className="text-9xl font-extrabold tracking-widest text-white max-md:text-8xl max-[38rem]:text-7xl max-[28rem]:text-6xl">
+            WEDDING
+          </span>
         </div>
         <span className="text-white text-2xl text-center font-semibold mt-10">
           THIỆP MỜI THAM DỰ LỄ THÀNH HÔN
@@ -22,11 +31,11 @@ export default function SectionFirts() {
             Nguyễn Nhàn
           </span>
         </div>
-        <div className="mt-7 w-32">
+        <div className="mt-12 w-32">
           <Separator className="my-2 bg-white" />
         </div>
 
-        <div className="mt-12 flex flex-col gap-6 items-center">
+        <div className="mt-4 flex flex-col gap-6 items-center">
           <span className="text-white text-xl tracking-[20px] font-medium font-sans max-[25rem]:tracking-[15px]">
             SAVE THE DATE
           </span>
